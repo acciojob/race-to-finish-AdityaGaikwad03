16573256914670326feda828127df17142836612
Aditya Gaikwad <128017788+AdityaGaikwad03@users.noreply.github.com>
2025-10-26 19:09:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+// An array to hold all promises
+
+for (let i = 1; i <= 5; i++) {
+  const randomTime = Math.floor(Math.random() * 5) + 1;
+
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise ${i} resolved in ${randomTime} seconds`);
+    }, randomTime * 1000);
+  });
+
+  promises.push(promise);
+}
+Promise.any(promises)
+  .then((result) => {
+    document.getElementById("output").innerText = result;
+  })
+  .catch((error) => {
+    document.getElementById("output").innerText = "All promises rejected!";
+  });
